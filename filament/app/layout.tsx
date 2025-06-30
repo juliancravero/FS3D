2f2ab6812ff71913cb63fee54385e5fadddcfbf8
Julian Cravero <juliancravero@hotmail.com>
2025-06-30 10:35:12 -0300

    Lucide icons

--- filament/app/layout.tsx
+++ filament/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Urbanist } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/ui/navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={urbanist.className} >  {children}
+        className={urbanist.className} >
+          <Navbar />
+          {children}
       </body>
     </html>
   );
---
+++ filament/components/ui/navbar.tsx
@@ -0,0 +1,34 @@
+"use client"
+import { useRouter } from "next/navigation";
+import { Heart, ShoppingCart, User } from "lucide-react";
+
+
+const Navbar = () => {
+    const router = useRouter()
+    return (
+        <div className='flex items-center justify-between p-4 nx-auto cursor-pointer sm:max-w-4xl md:max-w-6xl'>
+            <h1 className='text-3xl' onClick={() => router.push("/")}>Filament
+                <span className='dont-bold'> Solutions</span>
+            </h1>
+            <div className="items-center justify-between hidden sm:flex">
+                <p>Menu Desktop</p>
+            </div>
+            <div className="flex sm:hidden">
+                <p>Menu mobile</p>
+            </div>
+            <div className="flex items-center justify-between gap-2 sm:gap-7">
+                <ShoppingCart strokeWidth="1" 
+                    className="cursor-pointer" 
+                    onClick={() => router.push("/cart")}
+                />
+                <Heart strokeWidth="1"
+                    className="cursor-pointer" 
+                    onClick={() => router.push("/loved-products")}
+                />
+                <User strokeWidth={1} className="cursor-pointer" />
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
